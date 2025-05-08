f6d417580a80fce61df8cf6ddf2c5602741b4e36
georgepauly <georgepauly1995@gmail.com>
2025-05-08 09:52:18 +0530

    Added Error Handling and status codes

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask
 from src.controller import register_routes
 
 app = Flask(__name__)
+
 register_routes(app)
 
 @app.route('/')
 def home():
     return '<title>BbB Parking Lot!</title> <h1>BbB Parking Lot!</h1>', 200, {"Access-Control-Allow-Origin":"*"}
+
+@app.errorhandler(500)
+def internal_server_error():
+    return "Server Error", 500
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/controller.py
+++ src/controller.py
@@ -3,21 +3,24 @@ from flask import request
 from src.model import show_all_slots, show_ticket_details, update_exit_time, new_parking, show_number_plates, show_location
 
 def register_routes(app):
-
-    #4th and 6th APIs
     @app.route('/api/slots')
     def show_location_route():
         if request.args.get("number-plate") is None:
-            if show_all_slots() is None:
+            try:
+                all_slots = show_all_slots()
+                if not all_slots:
+                    return """<title>500 Internal Server Error</title>
+                            </head><body><h1>Internal Server Error</h1>
+                            <p>The server was unable to complete your request. Please try again later.</p>""", 500
+                return all_slots, 200, {"Access-Control-Allow-Origin":"*"}
+            except psycopg.errors.UndefinedTable:
                 return """<title>500 Internal Server Error</title>
                         </head><body><h1>Internal Server Error</h1>
                         <p>The server was unable to complete your request. Please try again later.</p>""", 500
-            return show_all_slots(), 200, {"Access-Control-Allow-Origin":"*"}
         else:
             number_plate = request.args.get("number-plate")
             return show_location(number_plate), 200, {"Access-Control-Allow-Origin":"*"}
 
-    #1st API
     @app.route('/api/tickets', methods=['POST'])
     def new_parking_route():
         new_parking_details = request.get_json()
@@ -26,7 +29,6 @@ def register_routes(app):
         car_color=new_parking_details["car_color"]
         return new_parking(number_plate, car_make, car_color), 200, {"Access-Control-Allow-Origin":"*"}
 
-    # 2nd API
     @app.route('/api/tickets/<int:ticketid>')
     def show_parking_details(ticketid):
         ticket_details = show_ticket_details(ticketid)
@@ -34,20 +36,20 @@ def register_routes(app):
             return {"message": "Invalid ticketid"}, 404
         return ticket_details, 200, {"Access-Control-Allow-Origin":"*"}
 
-    #3rd API
     @app.route('/api/tickets/<int:ticketid>', methods=['PATCH'])
     def update_parking(ticketid):
         ticket_closure = request.get_json()
-        if ticket_closure["status"] == "closed":
-            ticket_status = update_exit_time(ticketid)
-            if ticket_status == {"message": "Invalid ticketid"}:
-                return {"message": "Invalid ticketid"}, 404
-            elif ticket_status == {"message": "This ticket is already closed!"}:
-                return {"message": "This ticket is already closed!"}, 409
-            else:
-                return ticket_status, 200, {"Access-Control-Allow-Origin":"*"}
+        try:
+            if ticket_closure["status"] == "closed":
+                ticket_status = update_exit_time(ticketid)
+                if ticket_status == {"message": "Invalid ticketid"}:
+                    return {"message": "Invalid ticketid"}, 404
+                elif ticket_status == {"message": "This ticket is already closed!"}:
+                    return {"message": "This ticket is already closed!"}, 409
+                return ticket_status, 200, {"Access-Control-Allow-Origin":"*", "Access-Control-Allow-Methods":"*"}
+        except KeyError:
+            return 'Bad Request', 400
 
-    #5th API
     @app.route('/api/tickets')
     def show_number_plates_route():
         color = request.args.get("car-color")
